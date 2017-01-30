4de64f3a99815df946caa789409fbb082eb54d68
Toxicable <fabianwiles@live.com>
2017-01-30 18:39:50 +1300

    Updated angular-cli to beta.26 remade how the sample works, removed ngrx/store
    shifted member init to constructor

---
+++ samples/RefreshFlow/AngularApp/src/core/auth.service.ts
@@ -0,0 +1,169 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { RefreshGrantModel } from './models/refresh-grant-model';
+import { ProfileModel } from './models/profile-model';
+import { AuthStateModel } from './models/auth-state-model';
+import { AuthTokenModel } from './models/auth-tokens-model';
+import { RegisterModel } from './models/register-model';
+import { LoginModel } from './models/login-model';
+
+const jwtDecode = require('jwt-decode');
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/observable/throw';
+
+
+@Injectable()
+export class AuthService {
+
+    private initalState: AuthStateModel = { profile: null, tokens: null, authReady: false };
+    private authReady$ = new BehaviorSubject<boolean>(false);
+    state: BehaviorSubject<AuthStateModel>;
+    private refreshSubscription$: Subscription;
+
+    state$: Observable<AuthStateModel>;
+    tokens$: Observable<AuthTokenModel>;
+    profile$: Observable<ProfileModel>;
+    loggedIn$: Observable<boolean>;
+
+    constructor(
+        private http: Http,
+    ) {
+        this.state = new BehaviorSubject<AuthStateModel>(this.initalState);
+        this.state$ = this.state.asObservable();
+    }
+
+    init() {
+
+        this.tokens$ = this.state.filter(state => state.authReady)
+            .map(state => state.tokens);
+
+        this.profile$ = this.state.filter(state => state.authReady)
+            .map(state => state.profile);
+
+        this.loggedIn$ = this.tokens$.map(tokens => !!tokens);
+
+        return this.startupTokenRefresh()
+            .do(() => this.scheduleRefresh());
+    }
+
+    register(data: RegisterModel): Observable<Response> {
+        return this.http.post('http://localhost:5056/account/register', data)
+            .catch(res => Observable.throw(res.json()));
+    }
+
+    login(user: LoginModel) {
+        return this.getTokens(user, 'password')
+            .catch(res => Observable.throw(res.json()))
+            .do(res => this.scheduleRefresh());
+    }
+
+    logout() {
+        this.updateState(this.initalState);
+        if (this.refreshSubscription$) {
+            this.refreshSubscription$.unsubscribe();
+        }
+        this.removeToken()
+    }
+
+    isInRole(usersRole: string): Observable<boolean> {
+        return this.profile$
+            .map(profile => profile.role.find(role => role === usersRole) !== undefined);
+    }
+
+    refreshTokens() {
+        return this.state.first()
+            .map(state => state.tokens)
+            .flatMap(tokens => this.getTokens({ refresh_token: tokens.refresh_token }, 'refresh_token')
+                .catch(error => Observable.throw('Session Expired'))
+            );
+    }
+
+    private storeToken(tokens: AuthTokenModel) {
+        localStorage.setItem('auth-tokens', JSON.stringify(tokens));
+    }
+    private retrieveTokens() {
+        let tokensString = localStorage.getItem('auth-tokens');
+        let tokensModel: AuthTokenModel = tokensString == null ? null : JSON.parse(tokensString);
+        return tokensModel;
+    }
+    private removeToken() {
+        localStorage.removeItem('auth-tokens');
+    }
+
+    private updateState(newState: AuthStateModel) {
+        let previoudState = this.state.getValue();
+        this.state.next(Object.assign({}, previoudState, newState));
+    }
+
+    private getTokens(data: RefreshGrantModel | LoginModel, grantType: string) {
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers });
+
+        Object.assign(data, { grant_type: grantType, scope: 'openid offline_access' });
+
+        let params = new URLSearchParams();
+        Object.keys(data).forEach(key => params.append(key, (<any>data)[key]))
+
+        return this.http.post('http://localhost:5056/connect/token', params.toString(), options)
+            .map(res => {
+                let tokens: AuthTokenModel = res.json();
+                let now = new Date();
+                tokens.expiration_date = new Date(now.getTime() + tokens.expires_in * 1000).getTime().toString();
+
+                let profile: ProfileModel = jwtDecode(tokens.id_token);
+
+                this.storeToken(tokens);
+                this.updateState({ authReady: true, tokens, profile });
+                return res;
+            });
+    }
+
+
+
+    private startupTokenRefresh() {
+        return Observable.of(this.retrieveTokens())
+            .flatMap((tokens: AuthTokenModel) => {
+                // check if the token is even in localStorage, if it isn't tell them it's not and return
+                if (!tokens) {
+                    this.updateState({ authReady: true });
+                    return Observable.throw('No token in Storage');
+                }
+                let profile: ProfileModel = jwtDecode(tokens.id_token);
+                this.updateState({ tokens, profile });
+
+                if (+tokens.expiration_date > new Date().getTime()) {
+                    this.updateState({ authReady: true });
+                }
+
+                // it if is able to refresh then the getTokens method will let the app know that we're auth ready
+                return this.refreshTokens();
+            })
+            .catch(error => {
+                this.logout();
+                this.updateState({ authReady: true });
+                return Observable.throw(error);
+            });
+    }
+
+    private scheduleRefresh(): void {
+        this.refreshSubscription$ = this.tokens$
+            .first()
+            .flatMap(tokens => Observable.interval(tokens.expires_in / 2 * 1000))
+            .flatMap(() => this.refreshTokens())
+            .subscribe();
+    }
+}
+
--- samples/RefreshFlow/AngularApp/src/core/core.module.ts
+++ samples/RefreshFlow/AngularApp/src/core/core.module.ts
@@ -1,22 +1,10 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { AuthTokenService } from './auth-token/auth-token.service';
-import { AccountService } from './account/account.service';
-import { LoggedInActions } from './auth-store/logged-in.actions';
-import { ProfileActions } from './profile/profile.actions';
-import { AuthTokenActions } from './auth-token/auth-token.actions';
-import { AuthReadyActions } from './auth-store/auth-ready.actions';
-
+import { AuthService } from './auth.service';
 
 @NgModule({
     providers: [
-        Title,
-        AuthTokenService,
-        AccountService,
-        LoggedInActions,
-        ProfileActions,
-        AuthTokenActions,
-        AuthReadyActions,
+        AuthService
     ]
 
 })
